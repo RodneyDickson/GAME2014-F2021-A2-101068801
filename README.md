da8e98847e84c8005b2c79d1d0e5f695fa361686
RodneyDickson <43690520+RodneyDickson@users.noreply.github.com>
2021-10-24 21:57:10 -0400

    initial project commit
    submitting assets, settings, other project files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# GAME2014-F2021-A2-101068801
-Submission for Mobile Game Development I for Game 2014 - fall 2021 semester 
+# GAME2014-F2021-A1
+Assignment 1 submission for Game 2014 - Mobile Game Dev
+Rodney Dickson
+
+Initial commit includes: Main Menu with 3 buttons (Play, Instructions, Quit) which will all direct you somewhere. Play sends you to play scene, or main game scene but right now its static with no scripts. Instructions takes you to the instructions page with a home button included there. Quit game quits out of the application. 
---
+++ [Scripts]/SceneSwitcher.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void Home()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
